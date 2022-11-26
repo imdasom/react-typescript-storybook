87b66d6b52f13572825f923ca008fb98478b61fd
imdasom <imdasom101@gmail.com>
2022-11-26 20:14:54 +0900

    feat: 불필요한 초기세팅/파일 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function App(): JSX.Element {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        width: '100%',
+        height: '100vh',
+        display: 'grid',
+        gridAutoFlow: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <div>
+        <h1>React + Typescript + Storybook</h1>
+        <p>Component implementation project with storybook.</p>
+      </div>
     </div>
   );
 }
