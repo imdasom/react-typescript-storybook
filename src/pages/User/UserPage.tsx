9dba937cac204ae87cd59a9adcb7eb27c4a25e4c
imdasom <imdasom101@gmail.com>
2023-01-01 15:59:50 +0900

    feat: 회원관리 > 작업중 > 린트에러수정

--- src/pages/User/UserPage.tsx
+++ src/pages/User/UserPage.tsx
@@ -1,9 +1,6 @@
 import styles from './UserPage.module.scss';
-import { useEffect } from 'react';
 
 const UserPage = () => {
-  useEffect(() => {});
-
   return (
     <div className={styles.container}>
       <div className={styles.appContainer}>
